34acf633269adfd41340cbcaac47a6147353cffa
Frances Morales Velilla <frances0688@gmail.com>
2023-09-25 16:55:02 +0200

    all iterations complete

---
+++ script.js
@@ -0,0 +1,25 @@
+let cargador = [
+    "pium!",
+    "pium!",
+    "pium!",
+    "pium!",
+    "pium!",
+    "pium!",
+    "pium!"
+];
+
+function chauchat(arr){
+    let ramita = Math.random();
+    if (ramita > 0.8) {
+        return console.log("Illo, me he quedao pillá!");
+    }
+    for (i=0; i<arr.length; i++) {
+        if(i%3==0){
+            console.log('');
+        }
+        console.log(arr[i])
+    }
+}
+
+
+
